ce55f30b717a09594d5d73f5cc1d0ec9e38b7c49
Luis Perez <ext_lfperezv@falabella.cl>
2022-02-24 15:33:43 -0300

    se agrega go.mod

--- cmd/gopherapi/main.go
+++ cmd/gopherapi/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	//"github.com/friendsofgo/gopherapi/pkg/server"
 )
 
 func main() {
 
+	//s := server.New()
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	//log.Fatal(http.ListenAndServe(":8080", s.Router()))
+
+	// Instanciar tracer global Jaeger
+	//traceCfg, err := jaegercfg.conf config.FromEnv()
+
+	//_ = traceCfg
+	//_ = err
+
 }
